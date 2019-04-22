cf9dd50e214322bf67a41b948055dbd5fb1b07ca
Barry <530027054@qq.com>
2019-04-22 16:38:29 +0800

    修改背景图片加载地址问题

--- src/components/Global/index.vue
+++ src/components/Global/index.vue
@@ -1,12 +1,12 @@
 <script>
 
-  const serverUrl='/api';
+const serverUrl = '/api'
 
-  // const absoluteUrl='/api';
-  const absoluteUrl='http://bs-api.luckyme.com.cn';
-  export default
-  {
-    serverUrl,//Url地址
-    absoluteUrl,//绝对Url地址
-  }
+const absoluteUrl = '/api'
+// const absoluteUrl='http://bs-api.luckyme.com.cn';
+export default
+{
+  serverUrl, // Url地址
+  absoluteUrl// 绝对Url地址
+}
 </script>
